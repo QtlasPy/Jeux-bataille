c792744e9d8e7106a67b985dfac3f7f0f12faf7d
Atlas Py <140069667+QtlasPy@users.noreply.github.com>
2024-01-01 13:13:13 +0400

    Update main.py

--- Game/main.py
+++ Game/main.py
@@ -50,36 +50,37 @@ class Game:
                 self.bataille = False
 
     def update(self):
+        #Deroulement du jeux
         if not self.game and not self.win:
             self.game = self.Menu.lancer()
         elif self.game:
-            if not self.inGame:
+            if not self.inGame: #Debut d'une partie
                 self.jeuCartes.shuffle()
                 paquet1, paquet2 = Paquet(self.jeuCartes.jeux[:2], (120, 450)), Paquet(self.jeuCartes.jeux[-2:], (1030, 50))
                 self.playerEnJeu = (Player(self.Menu.names[0], paquet1), Player(self.Menu.names[1], paquet2))
                 self.inGame = True
             else:
-                if self.playerEnJeu[self.playerTour].paquet.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] and self.playerTour >= 0:
+                if self.playerEnJeu[self.playerTour].paquet.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] and self.playerTour >= 0: #L'orsque que le joueur a qui c'est le tour appuie sur son jeu. 
                     self.carteEnJeu.append(self.playerEnJeu[self.playerTour].tirer())
                     self.playerTour = 1 - self.playerTour
-
-
+    
+                #Lorsque les deux joueurs ont tirer une carte.
                 if not self.end_round and (len(self.carteEnJeu) == 2+self.batailleIndex and len(self.carteEnJeu) !=  0 and not self.bataille ):
                     self.playerTour = -1
                     self.nbTour += 1
                     self.playerWin = 0 if self.carteEnJeu[0 + self.batailleIndex].v > self.carteEnJeu[1 + self.batailleIndex].v else 1 if self.carteEnJeu[0 + self.batailleIndex].v < self.carteEnJeu[1 + self.batailleIndex].v else -2
-                    if self.playerWin in (0, 1):
+                    if self.playerWin in (0, 1): #Si il y'a un gagnant.
                         for carte in self.carteEnJeu:
                             self.playerEnJeu[self.playerWin].paquet.ajouter(carte)
                         self.end_round = True
                         self.bataille = False
 
-                    elif self.playerWin == -2:
+                    elif self.playerWin == -2: #Bataille !
                         self.bataille = True
                         self.end_round = True
                         self.batailleIndex += 2
-
-                if self.playerEnJeu[0].paquet.taille() == 0 or self.playerEnJeu[1].paquet.taille() == 0:
+            
+                if self.playerEnJeu[0].paquet.taille() == 0 or self.playerEnJeu[1].paquet.taille() == 0:  #Gestion de la fin d'une partie.
                     self.playerWin  = 1 if self.playerEnJeu[1].paquet.taille() == 0 else 2
                     self.win  = True
                     self.game = False
@@ -87,6 +88,7 @@ class Game:
 
 
     def display(self):
+        #Gestion de l'affichage
         time = pygame.time.get_ticks()
         self.screen.fill('white')
         if not self.game and not self.win:
